b9b2d0c96670a4a46306f7bac6cf1306d07a34d6
Zeeshan Meghji <zeeshan_meghji@hotmail.com>
2021-11-15 00:59:20 -0500

    separated contracts into separate files

--- contracts/SharedWallet.sol
+++ contracts/SharedWallet.sol
@@ -2,37 +2,7 @@
 
 pragma solidity ^0.8.9;
 
-import "@openzeppelin/contracts/access/Ownable.sol";
-
-contract Allowance is Ownable{
-
-    event AllowanceChanged(address indexed forWho, address indexed byWhom, 
-        uint oldAmount, uint newAmount);
-
-    event MoneySent(address indexed receiver, uint amount);
-    event MoneyReceived(address indexed sender, uint amount);
-
-    function isOwner() view internal returns (bool){
-        return owner() == msg.sender;
-    }
-
-    mapping (address => uint) allowances;
-
-    function changeAllowance(address toChange, uint amount ) public onlyOwner{
-        emit AllowanceChanged(toChange, msg.sender, allowances[toChange], amount);
-        allowances[toChange] = amount;
-    }
-
-    function reduceAllowance(address toReduce, uint amount) internal {
-        emit AllowanceChanged(toReduce, msg.sender, allowances[toReduce], allowances[toReduce]- amount);
-        allowances[toReduce] -= amount;
-    }
-
-    modifier ownerOrAllowed( uint amount){
-        require( isOwner() || amount <= allowances[msg.sender], "You are trying to withdraw more than your current allowance");
-        _;
-    }
-}
+import "./Allowance.sol";
 
 contract SharedWallet is Allowance{
 
