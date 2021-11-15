1cb51329119341e49267ceb0d260a7a342756500
Zeeshan Meghji <zeeshan_meghji@hotmail.com>
2021-11-14 22:01:21 -0500

    added onlyOwner modifier

--- contracts/SharedWallet.sol
+++ contracts/SharedWallet.sol
@@ -4,7 +4,17 @@ pragma solidity ^0.8.9;
 
 contract SharedWallet {
 
-    function withdrawMoney(address payable _to, uint _amount) public {
+    address owner;
+
+    constructor(){
+        owner = msg.sender;
+    }
+    
+    modifier onlyOwner(){
+        require(msg.sender != owner, "You are not the owner");
+        _;
+    }
+    function withdrawMoney(address payable _to, uint _amount) public onlyOwner{
         _to.transfer(_amount);
     }
 
