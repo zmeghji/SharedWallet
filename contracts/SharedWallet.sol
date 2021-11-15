4ffcc7f31cdb62f590326cdc0bc24c16c3d437b4
Zeeshan Meghji <zeeshan_meghji@hotmail.com>
2021-11-14 22:27:47 -0500

    use openzeppelin ownable contract

--- contracts/SharedWallet.sol
+++ contracts/SharedWallet.sol
@@ -2,23 +2,13 @@
 
 pragma solidity ^0.8.9;
 
-contract SharedWallet {
+import "@openzeppelin/contracts/access/Ownable.sol";
 
-    address owner;
+contract SharedWallet is Ownable{
 
-    constructor(){
-        owner = msg.sender;
-    }
-    
-    modifier onlyOwner(){
-        require(msg.sender != owner, "You are not the owner");
-        _;
-    }
     function withdrawMoney(address payable _to, uint _amount) public onlyOwner{
         _to.transfer(_amount);
     }
 
-    receive() external payable {
-
-    }
+    receive() external payable {}
 }
