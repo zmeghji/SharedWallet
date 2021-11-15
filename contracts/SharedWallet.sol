6a451bc7bf730c52ca390dc0e197c21944c63fa2
Zeeshan Meghji <zeeshan_meghji@hotmail.com>
2021-11-15 00:16:30 -0500

    tracked allowances of different addresses

--- contracts/SharedWallet.sol
+++ contracts/SharedWallet.sol
@@ -6,8 +6,23 @@ import "@openzeppelin/contracts/access/Ownable.sol";
 
 contract SharedWallet is Ownable{
 
-    function withdrawMoney(address payable _to, uint _amount) public onlyOwner{
-        _to.transfer(_amount);
+    function isOwner() view private returns (bool){
+        return owner() == msg.sender;
+    }
+    mapping (address => uint) allowances;
+
+    function changeAllowance(address addTo, uint amount ) public onlyOwner{
+        allowances[addTo] = amount;
+    }
+
+    modifier ownerOrAllowed( uint amount){
+        require( isOwner() || amount <= allowances[msg.sender], "You are trying to withdraw more than your current allowance");
+        _;
+    }
+
+    function withdrawMoney(address payable to, uint amount) public ownerOrAllowed(amount){
+        require(amount <= address(this).balance, "Not enough funds");
+        to.transfer(amount);
     }
 
     receive() external payable {}
